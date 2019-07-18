b1ec3c003ee8fc4debf3dd03a523155581291636
Carlos Machado <carlosedumachado@edu.univali.br>
2019-07-18 15:41:26 -0300

    pause update

--- EntreMundos/src/com/bakerystudios/gui/menu/PauseMenu.java
+++ EntreMundos/src/com/bakerystudios/gui/menu/PauseMenu.java
@@ -12,15 +12,14 @@ public class PauseMenu implements Updateble, Renderable {
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
 		
 	}
 	
 	@Override
 	public void render(Graphics g) {
-		//Graphics2D g2 = (Graphics2D) g;
-		g.setColor(new Color(0, 0, 0, 255));
-		g.fillRect(0, 0, Screen.SCALE_WIDTH, Screen.SCALE_HEIGHT);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(new Color(0, 0, 0, 150));
+		g2.fillRect(0, 0, Screen.SCALE_WIDTH, Screen.SCALE_HEIGHT);
 	}
 	
 }
