43b9c82928014a73775887b104feb420fa764d0f
Carlos Machado <carlosedumachado@edu.univali.br>
2019-07-18 08:54:58 -0300

    Adicionado background de texto

--- EntreMundos/src/com/bakerystudios/inventario/Inventario.java
+++ EntreMundos/src/com/bakerystudios/inventario/Inventario.java
@@ -37,40 +37,61 @@ public class Inventario implements Renderable, Updateble {
 
 	}
 
+	protected void drawCentralizedString(Graphics g, String str, int y) {
+		g.drawString(str, Screen.SCALE_WIDTH / 2 - g.getFontMetrics().stringWidth(str) / 2, y);
+	}
+	
+	protected void fillCentralizedRect(Graphics g, int y, int width, int height) {
+		g.fillRect(Screen.SCALE_WIDTH / 2 - width / 2, y, width, height);
+	}
+	
 	public void render(Graphics g) {
 		if (GameState.state == GameState.PLAYING && !Game.uiDoor  && !Game.uiNpc && visible) {
 			int numberMagic = 100;
+			Color db = new Color(111, 83, 39);
+			Color lb = new Color(190, 163, 115);
+
+			g.setFont(Game.boxFont);
+			
+			//g.setColor(db);
+			//fillCentralizedRect(g, 570, 500, 130);
+			//g.setColor(lb);
+			//fillCentralizedRect(g, 575, 490, 120);
+
+			g.setColor(Color.BLACK);
+			drawCentralizedString(g, "Exemplo de nome de item.", 605);
+
+			g.setFont(Game.inventFont);
 			for (int i = 0; i < numSlots; i++) {
-				g.setColor(Color.RED);
+				g.setColor(db);
 				g.drawRect(initialPosition + i * widthSlot, Screen.SCALE_HEIGHT - numberMagic, widthSlot, widthSlot);
-				g.setColor(Color.GRAY);
+				g.setColor(lb);
 				g.fillRect(initialPosition + i * widthSlot + 1, Screen.SCALE_HEIGHT - numberMagic + 1, widthSlot - 1,
 						widthSlot - 1);
 
-				g.setColor(Color.WHITE);
-				g.setFont(new Font("Arial", Font.BOLD, 18));
-				g.drawString(Integer.toString(slot[i].getAmount()), initialPosition + i * widthSlot + widthSlot - 12,
+				g.setColor(Color.BLACK);
+				g.drawString(Integer.toString(slot[i].getAmount()), initialPosition + i * widthSlot + widthSlot - 16,
 						Screen.SCALE_HEIGHT - numberMagic + widthSlot - 4);
 			}
 			// ITEM SELECIONADO
 			if(focus) {
-				g.setColor(Color.BLUE);
+				g.setColor(Color.BLACK);
 				g.drawRect(initialPosition + selectedItem * widthSlot, Screen.SCALE_HEIGHT - numberMagic, widthSlot, widthSlot);
-				g.setColor(Color.GRAY);
+				g.setColor(lb);
 				g.fillRect(initialPosition + selectedItem * widthSlot + 1, Screen.SCALE_HEIGHT - numberMagic + 1, widthSlot - 1,
 						widthSlot - 1);
-				g.setColor(Color.WHITE);
-				g.drawString(Integer.toString(slot[selectedItem].getAmount()), initialPosition + selectedItem * widthSlot + widthSlot - 12,
+				g.setColor(Color.BLACK);
+				g.drawString(Integer.toString(slot[selectedItem].getAmount()), initialPosition + selectedItem * widthSlot + widthSlot - 16,
 						Screen.SCALE_HEIGHT - numberMagic + widthSlot - 4);
 			}
 			if(Warehouse.exchangeInventory) {
-				g.setColor(Color.GREEN);
+				g.setColor(Color.RED);
 				g.drawRect(initialPosition + selectedItem * widthSlot, Screen.SCALE_HEIGHT - numberMagic, widthSlot, widthSlot);
-				g.setColor(Color.GRAY);
+				g.setColor(lb);
 				g.fillRect(initialPosition + selectedItem * widthSlot + 1, Screen.SCALE_HEIGHT - numberMagic + 1, widthSlot - 1,
 						widthSlot - 1);
-				g.setColor(Color.WHITE);
-				g.drawString(Integer.toString(slot[selectedItem].getAmount()), initialPosition + selectedItem * widthSlot + widthSlot - 12,
+				g.setColor(Color.BLACK);
+				g.drawString(Integer.toString(slot[selectedItem].getAmount()), initialPosition + selectedItem * widthSlot + widthSlot - 16,
 						Screen.SCALE_HEIGHT - numberMagic + widthSlot - 4);
 			}
 		}
